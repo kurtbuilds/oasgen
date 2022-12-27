e92f3565021476ee6f9bf07af6724a5c185f3703
Kurt Wolf <kurtwolfbuilds@gmail.com>
2022-12-26 21:13:06 -0500

    wip

--- oasgen/examples/simple.rs
+++ oasgen/examples/simple.rs
@@ -17,33 +17,36 @@ async fn send_code(_body: Json<SendCode>) -> Json<SendCodeResponse> {
     Json(SendCodeResponse { found_account: false })
 }
 
+fn collector<F, Args>(_handler: F) -> () where
+    F: actix_web::Handler<Args>,
+    Args: actix_web::FromRequest + 'static,
+    F::Output: actix_web::Responder + 'static,
+{
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     use std::fs::File;
     use actix_web::{HttpResponse, web, HttpServer, App};
 
-    let s = Server::new()
-        .get("/hello", send_code)
-
-        ;
-    let res = send_code(Json(SendCode { mobile: "123".to_string() })).await;
-    println!("{:#?}", res);
     serde_yaml::to_writer(&File::create("examples/simple.yaml").unwrap(), &s.openapi).unwrap();
 
     let host = "0.0.0.0";
     let port = 5000;
     let host = format!("{}:{}", host, port);
 
-    let server = Server::new()
-        .post("/send-code", send_code)
-        ;
+    collector(send_code);
+
+    // let server = Server::new()
+    //     .post("/send-code", send_code)
+    //     ;
         // .into_service();
 
     // HttpServer::new(move || App::new()
-    App::new()
-        .route("/healthcheck", web::get().to(|| async { HttpResponse::Ok().body("Ok") }))
-        .route("/send-code", web::post().to(send_code))
-    ;
+    // App::new()
+    //     .route("/healthcheck", web::get().to(|| async { HttpResponse::Ok().body("Ok") }))
+    //     .route("/send-code", web::post().to(send_code))
+    // ;
     Ok(())
         // .service(server.clone().create_service())
         // .service(build_openapi().into_service("/api"))
