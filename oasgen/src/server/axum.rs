30ac9bf7e0b4448c4990887d8fdebd51bc199f47
Kurt Wolf <kurtwolfbuilds@gmail.com>
2023-04-15 15:15:11 -0400

    remove println

--- oasgen/src/server/axum.rs
+++ oasgen/src/server/axum.rs
@@ -120,10 +120,8 @@ impl<S> Server<Router<S>, Arc<OpenAPI>>
 
         #[cfg(feature = "swagger-ui")]
         if let Some(mut path) = self.swagger_ui_route {
-            println!("Swagger UI route: {}", path);
             let swagger = self.swagger_ui.expect("Swagger UI route set but no Swagger UI is configured.");
             let handler = routing::get(|uri: http::Uri| async move {
-                println!("Swagger UI request: {}", uri);
                 match swagger.handle_url(&uri) {
                     Some(response) => response.into_response(),
                     None => {
