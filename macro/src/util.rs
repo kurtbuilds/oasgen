2ff24309045de531289fda7e4876fb8036d93199
Erik Williams <thorjelly@gmail.com>
2023-09-04 16:29:13 -0700

    Added derivable newtypes for OaSchema macro under the condition that a struct's fields are unnamed and there is only one field

--- macro/src/util.rs
+++ macro/src/util.rs
@@ -1,18 +1,76 @@
-use syn::punctuated::Punctuated;
-use syn::token::Comma;
-use syn::Data::Struct;
-use syn::{DataStruct, DeriveInput, Field, Fields, FieldsNamed};
-
+use oasgen_core::OpenApiAttributes;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{punctuated::Punctuated, token::Comma, *};
 
 /// Given derive input of a struct, get the fields of the struct.
-pub fn get_fields(ast: &DeriveInput) -> &Punctuated<Field, Comma> {
-    let fields = match &ast.data {
-        Struct(DataStruct { ref fields, .. }) => fields,
-        _ => panic!("#[ormlite] can only be used on structs"),
+pub fn derive_oaschema_struct(ident: &Ident, fields: &Punctuated<Field, Comma>) -> TokenStream {
+    let fields: Vec<(&syn::Field, OpenApiAttributes)> = fields
+        .into_iter()
+        .map(|f| (f, OpenApiAttributes::try_from(&f.attrs).unwrap()))
+        .collect::<Vec<_>>();
+
+    let properties = fields.iter().map(|(f, attr)| {
+                    if attr.skip {
+                        return quote! {};
+                    }
+                    let name = f.ident.as_ref().unwrap().to_string();
+                    let ty = &f.ty;
+                    quote! {
+                        o.add_property(#name, <#ty as OaSchema>::schema().expect(concat!("No schema found for ", #name))).unwrap();
+                    }
+                });
+
+    let required = fields.iter().map(|(f, attr)| {
+        if attr.skip || attr.skip_serializing_if.is_some() {
+            return quote! {};
+        }
+        let name = f.ident.as_ref().unwrap().to_string();
+        quote! { #name.to_string(), }
+    });
+    let required = quote! { vec! [ #(#required)* ] };
+
+    let name = ident.to_string();
+    let ref_name = format!("#/components/schemas/{}", ident);
+    let expanded = quote! {
+        impl ::oasgen::OaSchema for #ident {
+            fn schema_name() -> Option<&'static str> {
+                Some(#name)
+            }
+
+            fn schema_ref() -> Option<::oasgen::ReferenceOr<::oasgen::Schema>> {
+                Some(::oasgen::ReferenceOr::ref_(#ref_name))
+            }
+
+            fn schema() -> Option<::oasgen::Schema> {
+                let mut o = ::oasgen::Schema::new_object();
+                #(#properties)*
+                let req = o.required_mut().unwrap();
+                *req = #required;
+                Some(o)
+            }
+        }
     };
-    let fields = match fields {
-        Fields::Named(FieldsNamed { named, .. }) => named,
-        _ => panic!("#[ormlite] can only be used on structs with named fields"),
+    TokenStream::from(expanded)
+}
+
+pub fn derive_oaschema_newtype(ident: &Ident, field: &Field) -> TokenStream {
+    let ty = &field.ty;
+    let name = ident.to_string();
+    let expanded = quote! {
+        impl ::oasgen::OaSchema for #ident {
+            fn schema_name() -> Option<&'static str> {
+                Some(<#ty as OaSchema>::schema_name().expect(concat!("No schema name found for ", #name)))
+            }
+
+            fn schema_ref() -> Option<::oasgen::ReferenceOr<::oasgen::Schema>> {
+                Some(<#ty as OaSchema>::schema_ref().expect(concat!("No schema ref found for ", #name)))
+            }
+
+            fn schema() -> Option<::oasgen::Schema> {
+                Some(<#ty as OaSchema>::schema().expect(concat!("No schema found for ", #name)))
+            }
+        }
     };
-    fields
+    TokenStream::from(expanded)
 }
--- oasgen/tests/test-none.rs
+++ oasgen/tests/test-none.rs
@@ -3,4 +3,5 @@ fn run_tests() {
     let t = trybuild::TestCases::new();
     t.pass("tests/test-none/01-hello.rs");
     t.pass("tests/test-none/02-required.rs");
+    t.pass("tests/test-none/03-newtype.rs");
 }
