79dbb11cda011edb577a17581ada43c41ba20b07
thorjelly <thorjelly@gmail.com>
2023-12-03 10:12:18 -0800

    Impl oaschema chrono fixed offset (#7)
    * Implemented OaSchema on HashMap
    
    * Added serde_qs as a feature. Serde_qs provides QsQuery, which is a popular alternative to actix_web::web::Query that provides better parsing for query arrays.
    
    * Added qs feature to oasgen
    
    * Added DateTime<FixedOffset> to implement OaSchema
    
    * Renamed feature from qs to serde_qs
    
    * update ignore with .vscode
    
    ---------
    
    Co-authored-by: Kurt Wolf <kurtwolfbuilds@gmail.com>

--- core/src/schema/chrono.rs
+++ core/src/schema/chrono.rs
@@ -2,4 +2,5 @@ use crate::impl_oa_schema;
 
 impl_oa_schema!(::chrono::NaiveDate, crate::Schema::new_string().with_format("date"));
 impl_oa_schema!(::chrono::DateTime<::chrono::Utc>, crate::Schema::new_string().with_format("date-time"));
+impl_oa_schema!(::chrono::DateTime<::chrono::FixedOffset>, crate::Schema::new_string().with_format("date-time"));
 impl_oa_schema!(::chrono::NaiveDateTime, crate::Schema::new_string().with_format("date-time"));
