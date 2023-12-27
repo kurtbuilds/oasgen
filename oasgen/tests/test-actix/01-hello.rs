8323f2a8f88432d8df83867002b55ce7fadd66e3
Kurt Wolf <kurtwolfbuilds@gmail.com>
2023-12-27 12:38:59 -0500

    add regression test for #11

--- oasgen/tests/test-actix/01-hello.rs
+++ oasgen/tests/test-actix/01-hello.rs
@@ -1,6 +1,7 @@
 use oasgen::{OaSchema, Server, oasgen};
-use actix_web::web::Json;
+use actix_web::web::{Json, Query};
 use serde::{Deserialize, Serialize};
+
 
 #[derive(Deserialize, OaSchema)]
 pub struct SendCode {
@@ -17,12 +18,31 @@ async fn send_code(_body: Json<SendCode>) -> Json<SendCodeResponse> {
     Json(SendCodeResponse { found_account: false })
 }
 
+#[derive(Deserialize, OaSchema)]
+pub struct GetCode {
+    pub code: String,
+}
+
+#[derive(Serialize, OaSchema)]
+pub struct CodeResponse {
+    pub found_code: bool,
+}
+
+#[oasgen]
+async fn get_code(Query(GetCode { code }): Query<GetCode>) -> Json<CodeResponse> {
+    Json(CodeResponse {
+        found_code: matches!(&*code, "1234" | "5678"),
+    })
+}
+
 fn main() {
     use pretty_assertions::assert_eq;
     let server = Server::actix()
         .post("/hello", send_code)
+        .get("/get-code", get_code)
         ;
     let spec = serde_yaml::to_string(&server.openapi).unwrap();
+    println!("{}", spec);
     let other = include_str!("01-hello.yaml");
     assert_eq!(spec.trim(), other);
 }
