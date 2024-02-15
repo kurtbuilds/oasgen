ed97d8c41a5abe3b345a2c4018af2f37c8cf4303
Pascal Bach <pascal.bach@siemens.com>
2024-02-15 15:56:23 +0100

    add axum Path extraction capability
    This turns a axum::extract::Path into a parameter

--- oasgen/tests/test-axum.rs
+++ oasgen/tests/test-axum.rs
@@ -3,4 +3,5 @@ fn run_tests() {
     let t = trybuild::TestCases::new();
     t.pass("tests/test-axum/01-hello.rs");
     t.pass("tests/test-axum/02-query.rs");
+    t.pass("tests/test-axum/03-path.rs");
 }
---
+++ oasgen/tests/test-axum/03-path.rs
@@ -0,0 +1,29 @@
+use axum::extract::{Path, Json};
+use oasgen::{OaSchema, oasgen, Server};
+use serde::{Deserialize};
+
+/// Send a code to a mobile number
+#[derive(Deserialize, OaSchema)]
+pub struct TaskFilter {
+    pub completed: bool,
+    pub assigned_to: i32,
+}
+
+#[oasgen]
+async fn get_task(Path(_id): Path<u64>) -> Json<()> {
+    Json(())
+}
+
+fn main() {
+    use pretty_assertions::assert_eq;
+    let server = Server::axum()
+        .get("/tasks/:id/", get_task)
+        ;
+
+    let spec = serde_yaml::to_string(&server.openapi).unwrap();
+    let other = include_str!("03-path.yaml");
+    assert_eq!(spec.trim(), other);
+    let router = axum::Router::new()
+        .merge(server.freeze().into_router());
+    router.into_make_service();
+}
