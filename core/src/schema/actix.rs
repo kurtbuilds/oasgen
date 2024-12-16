5fc1bfeed6edf7409e784ab6e6a6f7f5c747382d
Markus Bergum Hilbertsen <markus@orcalabs.no>
2024-12-16 13:05:37 +0100

    refactor: OaParameter impls for actix types

--- core/src/schema/actix.rs
+++ core/src/schema/actix.rs
@@ -18,12 +18,23 @@ impl OaSchema for actix_web::HttpResponse {
 impl<T> OaParameter for actix_web::web::Data<T> {}
 impl OaParameter for actix_web::HttpRequest {}
 
-
 impl<T: OaParameter> OaParameter for actix_web::web::Path<T> {
     fn parameters() -> Vec<RefOr<oa::Parameter>> {
         T::parameter_schemas()
             .into_iter()
-            .map(|s| RefOr::Item(oa::Parameter::path("path", s)))
+            .flat_map(|s| s.into_item())
+            .enumerate()
+            .flat_map(|(i, s)| -> Box<dyn Iterator<Item = _>> {
+                match s.kind {
+                    SchemaKind::Type(Type::Object(o)) => Box::new(o.properties.into_iter()),
+                    _ => Box::new(Some((format!("path{i}"), RefOr::Item(s))).into_iter()),
+                }
+            })
+            .map(|(k, v)| {
+                let mut param = oa::Parameter::path(k, v);
+                param.required = true;
+                RefOr::Item(param)
+            })
             .collect()
     }
 }
@@ -34,8 +45,8 @@ impl<T: OaParameter> OaParameter for actix_web::web::Query<T> {
             .into_iter()
             .flat_map(|s| s.into_item())
             .flat_map(|s| match s.kind {
-                SchemaKind::Type(Type::Object(o)) => { Some(o.properties) }
-                _ => None
+                SchemaKind::Type(Type::Object(o)) => Some(o.properties),
+                _ => None,
             })
             .flatten()
             .map(|(k, v)| RefOr::Item(oa::Parameter::query(k, v)))
@@ -46,9 +57,6 @@ impl<T: OaParameter> OaParameter for actix_web::web::Query<T> {
 #[cfg(feature = "qs")]
 impl<T: OaParameter> OaParameter for serde_qs::actix::QsQuery<T> {
     fn parameters() -> Vec<RefOr<oa::Parameter>> {
-        T::parameter_schemas()
-            .into_iter()
-            .map(|s| RefOr::Item(oa::Parameter::query("query", s)))
-            .collect()
+        actix_web::web::Query::<T>::parameters()
     }
 }
